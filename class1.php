e259142b7e1d323bd503467dbd52088627b195e7
MoonKabir <45933210+moonkabir@users.noreply.github.com>
2020-08-25 23:47:38 +0600

    Add files via upload

---
+++ class1.php
@@ -0,0 +1,39 @@
+<?php
+
+$Name = "Bangladesh";
+
+
+echo "This is my First Project".
+PHP_EOL
+."This is a multiple-lines comment block".
+PHP_EOL
+//that spans over multiple
+."lines\n";
+echo $Name;
+
+print "<h2>PHP is Fun!</h2>";
+
+$x = 5985;
+var_dump($x);
+var_dump($Name);
+
+// $cal = array("")
+
+
+
+
+
+$cars = array("Volvo","BMW","Toyota",6666);
+var_dump($cars);
+
+$data = $cars[2];
+echo $data;
+
+
+
+$x = '';
+var_dump($x);
+
+echo strlen("Hello world!");
+
+?>
---
+++ class2.php
@@ -0,0 +1,45 @@
+<?php
+// echo (pi());
+// $min = min(0, 150, 30, 20, -8, -200);  // returns -200
+// printf($min);
+// $min = min(0, 150, 30, 20, -8, -200);  // returns -200
+// printf($min);
+
+// echo(min(0, 150, 30, 20, -8, -200));
+// echo(max(0, 150, 30, 20, -8, -200));
+
+// echo(round(0.60));
+
+// echo(rand(10,100));
+
+// define("cars", [
+//   "Alfa Romeo",
+//   "BMW",
+//   "Toyota"
+// ]);
+// echo cars[0];
+
+// $x=6;
+// echo $x;
+// echo PHP_EOL;
+// $x=7;
+// echo $x;
+// echo PHP_EOL;
+// define("y",[6]);
+// define("y",[7]);
+// echo y[0];
+
+// echo (10**3);
+// $x='6';
+// if(!$y){
+//     echo "my name is moon";
+// }
+
+// echo $status = (empty($user)) ? "anonymous" : "logged in";
+// ternary operator
+// condition?true value : false  value;
+// ["user"] ?? "anonymous";
+$color = "red";
+// $color ?? "red";
+// nullcolas operator
+echo $color = $color ?? "blue";
